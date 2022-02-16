87853256d3debc54539913cc439710d6c8ac25c2
Minami Tomonobu <minami@edanoue.com>
2022-02-16 19:48:21 +0900

    fix serializedObjectField

--- Editor/Asmdef2PlantUmlWindow.cs
+++ Editor/Asmdef2PlantUmlWindow.cs
@@ -10,12 +10,22 @@ namespace asmdef2pu
 {
     public class Asmdef2PlantUmlWindow : EditorWindow
     {
+        SerializedObject? _thisSerializedObject = null;
+        SerializedObject ThisSerializedObject
+        {
+            get
+            {
+                if (_thisSerializedObject is null)
+                    _thisSerializedObject = new(this);
+                return _thisSerializedObject;
+            }
+        }
         ExportOptions _options = new();
         string _textResultPlantUml = "";
         Vector2 _scroll;
 
         [SerializeField]
-        private List<string> _無視するディレクトリのリスト = new();
+        private List<string> _excludeDirectoryPattern = new() { "Assets/Plugins" };
 
         [MenuItem("Tools/Asmdef2PlantUML")]
         private static void ShowWindow()
@@ -25,33 +35,42 @@ namespace asmdef2pu
             window.Show();
         }
 
+
         void OnGUI()
         {
-            // Draw Options
+            // Style Options
             {
-                // 自身のSerializedObjectを取得
-                // リストのために必要
-                var so = new SerializedObject(this);
-                so.Update();
-
                 GUILayout.Label("Style Options", EditorStyles.boldLabel);
                 _options.bNestedNamespace = EditorGUILayout.ToggleLeft("Assembly 名のドット区切りで Namespace を作成", _options.bNestedNamespace);
+            }
+
+            GUILayout.Space(10);
+
+            // Target Options
+            {
                 GUILayout.Label("Target Options", EditorStyles.boldLabel);
                 _options.bIgnorePackageAssembly = EditorGUILayout.ToggleLeft("Package/ 以下の Assembly は除外", _options.bIgnorePackageAssembly);
                 _options.bIgnoreUnityAssembly = EditorGUILayout.ToggleLeft("Unity の Assembly は除外", _options.bIgnoreUnityAssembly);
                 _options.bIgnoreAssemblyCSharp = EditorGUILayout.ToggleLeft("Assembly-CSharp は除外", _options.bIgnoreAssemblyCSharp);
-                { // 除外リスト
-                    // 第二引数をtrueにしたPropertyFieldで描画
-                    var a = so.FindProperty(nameof(_無視するディレクトリのリスト));
-                    EditorGUILayout.PropertyField(a, true);
+
+                // 除外リスト
+                {
+                    ThisSerializedObject.Update();
+                    var property = ThisSerializedObject.FindProperty(nameof(_excludeDirectoryPattern));
+                    EditorGUILayout.PropertyField(property: property, label: new GUIContent("無視するディレクトリのリスト"), includeChildren: true);
+                    ThisSerializedObject.ApplyModifiedProperties();
                 }
+            }
+
+            GUILayout.Space(10);
+
+            // Dependency Options
+            {
+
                 GUILayout.Label("Dependency Options", EditorStyles.boldLabel);
                 _options.bIgnoreUnityAssemblyDependency = EditorGUILayout.ToggleLeft("Unity の Assembly は依存元から除く", _options.bIgnoreUnityAssemblyDependency);
                 _options.bIgnoreUnityEngineUiDependency = EditorGUILayout.ToggleLeft("UnityEngine.UI への依存は注釈にする", _options.bIgnoreUnityEngineUiDependency);
 
-
-                // リストのために必要
-                so.ApplyModifiedProperties();
             }
 
             GUILayout.Space(10);
@@ -60,7 +79,7 @@ namespace asmdef2pu
             if (GUILayout.Button("Generate PlantUML Text"))
             {
                 // こっちでだけ持ってるオプションがあるので 手動で入れる
-                _options.ignoreDirectoryPatterns = this._無視するディレクトリのリスト;
+                _options.ignoreDirectoryPatterns = this._excludeDirectoryPattern;
                 _textResultPlantUml = Generator.Generate(_options);
             }
 
